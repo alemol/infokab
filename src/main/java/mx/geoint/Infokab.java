6b3dc39b0f87aef229a48c28eed9f2026593c924
Jose Luis <ucp.jose@gmail.com>
2022-05-11 13:11:47 -0500

    Inicializa una aplicación spring

---
+++ src/main/java/mx/geoint/Infokab.java
@@ -0,0 +1,11 @@
+package mx.geoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Infokab {
+    public static void main(String[] args) {
+        SpringApplication.run(Infokab.class, args);
+    }
+}
