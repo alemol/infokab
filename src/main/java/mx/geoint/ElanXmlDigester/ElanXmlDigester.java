92eaf30ad64f59db62e6a6b938ce9725e0c68770
sergio <sage_sergio@hotmail.com>
2022-06-02 15:24:14 -0500

    Actualización de path en el sistema
    Former-commit-id: 2985c76920dfbe5f632d886a894813cd751595d9

--- src/main/java/mx/geoint/ElanXmlDigester/ElanXmlDigester.java
+++ src/main/java/mx/geoint/ElanXmlDigester/ElanXmlDigester.java
@@ -4,8 +4,10 @@ import com.google.gson.Gson;
 import mx.geoint.FFmpeg.FFmpeg;
 import mx.geoint.ParseXML.ParseXML;
 import mx.geoint.ParseXML.Tier;
+import mx.geoint.pathSystem;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class ElanXmlDigester {
@@ -16,27 +18,37 @@ public class ElanXmlDigester {
         filepath = eaf_path;
     }
 
-    public void parse_tier(String tier_id, boolean save_text, boolean save_media){
+    public void parse_tier(String tier_id, boolean save_text, boolean save_media) throws IOException {
         ParseXML parseXML = new ParseXML(filepath, tier_id);
         parseXML.read();
-        FFmpeg ffmpeg = new FFmpeg("src/main/resources/");
+        FFmpeg ffmpeg = new FFmpeg(pathSystem.DIRECTORY_MULTIMEDIA);
         getTier = parseXML.getTier();
         for (int i = 0; i< getTier.size();i++){
             Tier tier = getTier.get(i);
-            System.out.println(getTier.get(i).ANNOTATION_ID);
-            boolean creado = ffmpeg.cortador("eligio_uikab_mena.wav",
+            String path = "eligio_uikab_mena";
+            String type_path = "wav";
+            String file_name = format_name(tier, tier_id, path, type_path);
+            String file_name_json = format_name(tier, tier_id, path,"json");
+
+            boolean creado = ffmpeg.cortador(path,
                     (Integer.parseInt(tier.TIME_VALUE1)/1000),
                     (tier.DIFF_TIME/1000)+.5,
-                    tier.ANNOTATION_ID+"_"+tier_id+"_"+tier.TIME_SLOT_REF1+"_"+tier.TIME_SLOT_REF2+"_"+tier.TIME_VALUE1+"_"+tier.TIME_VALUE2+"_eligio_uikab_mena.wav");
+                    file_name);
+
             if(creado==true){
                 Gson gson = new Gson();
-                FileWriter file = new FileWriter(tier.ANNOTATION_ID+".json");
+                FileWriter file = new FileWriter(pathSystem.DIRECTORY_FILES_JSON+file_name_json);
                 file.write(gson.toJson(tier));
                 file.close();
             }
         }
     }
 
+    public String format_name(Tier tier, String tier_id, String path, String type_file){
+        String name_file = String.format("%s_%s_%s_%s_%s_%s_%s.%s", tier.ANNOTATION_ID, tier_id, tier.TIME_SLOT_REF1, tier.TIME_SLOT_REF2, tier.TIME_VALUE1, tier.TIME_VALUE2, path, type_file);
+        System.out.println(name_file);
+        return name_file;
+    }
 }
 
     /*;*/
