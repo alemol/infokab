58d408af1d29206e728768793099d8865ed55258
ulises <umorales@centrogeo.edu.mx>
2022-06-02 16:58:06 -0500

    modificada la respuesta al crear un archivo
    Former-commit-id: fc7687683f5d7c9445a8f308ca8f52173412a172

--- src/main/java/mx/geoint/pathSystem.java
+++ src/main/java/mx/geoint/pathSystem.java
@@ -1,7 +1,7 @@
 package mx.geoint;
 
 public class pathSystem {
-    public static final String DIRECTORY_MULTIMEDIA = "/home/alejandro/Documentos/projects/infokab-backend/Files/multimedia/";
-    public static final String DIRECTORY_FILES_JSON = "/home/alejandro/Documentos/projects/infokab-backend/Files/file_to_index_json/";
-    public static final String DIRECTORY_INDEX_LUCENE = "/home/alejandro/Documentos/projects/infokab-backend/Files/index/";
+    public static final String DIRECTORY_MULTIMEDIA = "/home/ulises/eclipse-workspace/infokab-backend/Files/multimedia/";
+    public static final String DIRECTORY_FILES_JSON = "/home/ulises/eclipse-workspace/infokab-backend/Files/file_to_index_json/";
+    public static final String DIRECTORY_INDEX_LUCENE = "/home/ulises/eclipse-workspace/infokab-backend/Files/index/";
 }
