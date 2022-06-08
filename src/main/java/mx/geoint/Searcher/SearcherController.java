0a98f26e636b0750ead2768d37ef3be216b2c521
Jose Luis <ucp.jose@gmail.com>
2022-06-08 09:48:02 -0500

    Agregada ruta en la api para búsqueda por palabra

--- src/main/java/mx/geoint/Searcher/SearcherController.java
+++ src/main/java/mx/geoint/Searcher/SearcherController.java
@@ -1,8 +1,13 @@
 package mx.geoint.Searcher;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/search")
@@ -17,7 +22,16 @@ public class SearcherController {
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
-    public void search(String searchValue){
-        searcherService.findDocuments(searchValue);
+    public List<Document> search(@RequestBody String text){
+        try{
+            List<Document> documents = searcherService.findDocuments(text);
+            return documents;
+        }
+        catch (IOException ex){
+            return null;
+        }
+        catch (ParseException ex){
+            return null;
+        }
     }
 }
--- src/main/java/mx/geoint/Searcher/SearcherService.java
+++ src/main/java/mx/geoint/Searcher/SearcherService.java
@@ -1,7 +1,12 @@
 package mx.geoint.Searcher;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class SearcherService {
@@ -9,7 +14,7 @@ public class SearcherService {
     @Autowired
     Searcher searcher;
 
-    public String[] findDocuments(String searchValue){
+    public List<Document> findDocuments(String searchValue) throws IOException, ParseException {
         return searcher.find(searchValue);
     }
 }
