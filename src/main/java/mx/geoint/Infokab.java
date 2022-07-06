fa5658efac009f1490bd16243df9d5a930d751c0
sergio <sage_sergio@hotmail.com>
2022-07-06 09:58:41 -0500

    Actualización de ruta relativa en pathsystem

--- src/main/java/mx/geoint/Infokab.java
+++ src/main/java/mx/geoint/Infokab.java
@@ -13,17 +13,6 @@ public class Infokab {
     public static void main(String[] args) {
         SpringApplication.run(Infokab.class, args);
     }
-
-    @RequestMapping(value = "/cutAudio/{source}/{start}/{duration}/{output}", method = GET)
-    @ResponseBody
-    public void cutAudio(@PathVariable String source, @PathVariable double start, @PathVariable double duration, @PathVariable String output) {
-
-            FFmpeg multimedia = new FFmpeg("src/main/resources/");
-
-            multimedia.cortador(source,start,duration,"a5_Transcripción_ts1_ts5_34240_39000_eligio_uikab_mena.wav");
-
-
-    }
 }
 
 
--- src/main/java/mx/geoint/pathSystem.java
+++ src/main/java/mx/geoint/pathSystem.java
@@ -1,7 +1,8 @@
 package mx.geoint;
 
 public class pathSystem {
-    public static final String DIRECTORY_MULTIMEDIA = "/home/ulises/eclipse-workspace/infokab-backend/Files/multimedia/";
-    public static final String DIRECTORY_FILES_JSON = "/home/ulises/eclipse-workspace/infokab-backend/Files/file_to_index_json/";
-    public static final String DIRECTORY_INDEX_LUCENE = "/home/ulises/eclipse-workspace/infokab-backend/Files/index/";
+    public static final String DIRECTORY_MULTIMEDIA = "./Files/multimedia/";
+    public static final String DIRECTORY_FILES_JSON = "./Files/file_to_index_json/";
+    public static final String DIRECTORY_INDEX_LUCENE = "./Files/index/";
+    public static final String DIRECTORY_INDEX_GENERAL = "./Files/Indices/";
 }
