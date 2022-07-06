8df109d2a7ae89890bd7bb6eaf192f5a05219c4e
unknown <ucp.jose@gmail.com>
2022-07-06 10:42:17 -0500

    Rutas de parthsystem a relativas

--- src/main/java/mx/geoint/pathSystem.java
+++ src/main/java/mx/geoint/pathSystem.java
@@ -1,7 +1,9 @@
 package mx.geoint;
 
 public class pathSystem {
-    public static final String DIRECTORY_MULTIMEDIA = "/home/jose/Documentos/CentroGeo/Infokab/Files/multimedia/";
-    public static final String DIRECTORY_FILES_JSON = "/home/jose/Documentos/CentroGeo/Infokab/Files/file_to_index_json/";
-    public static final String DIRECTORY_INDEX_LUCENE = "/home/jose/Documentos/CentroGeo/Infokab/Files/index/";
+    public static final String DIRECTORY_MULTIMEDIA = "./Files/multimedia/";
+    public static final String DIRECTORY_FILES_JSON = "./Files/file_to_index_json/";
+    public static final String DIRECTORY_INDEX_LUCENE = "./Files/index/";
+    public static final String DIRECTORY_INDEX_GENERAL = "./Files/Indices/";
+
 }
