7749eec58ed7771a1d53f72c75166311e7518ce4
sergio <sage_sergio@hotmail.com>
2022-07-29 11:20:41 -0500

    Actualización del nombre del proyecto concatenando la fecha, y agregadó codigos de error en api upload

--- src/main/java/mx/geoint/pathSystem.java
+++ src/main/java/mx/geoint/pathSystem.java
@@ -5,4 +5,8 @@ public class pathSystem {
     public static final String DIRECTORY_INDEX_GENERAL = "./Files/Indices/";
 
     public static final String DIRECTORY_PROJECTS = "./Files/Project/";
+
+    public static final Number SUCCESS_UPLOAD = 00;
+    public static final Number NOT_UPLOAD_EAF_FILE = 01;
+    public static final Number NOT_UPLOAD_MULTIMEDIA_FILE = 02;
 }
