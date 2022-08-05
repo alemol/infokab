da2613fe924bbf92e32634f2003b4fad498ea31e
Ulises <umorales@centrogeo.edu.mx>
2022-08-05 08:28:13 -0500

    lista de recortes
    Former-commit-id: 1725c0dee01dbd4650380d9fb5fbc6f5c735f6eb
    Former-commit-id: 8306b30a85fb2e2f84a52ce05cd6d901a3f5f81d

--- src/main/java/mx/geoint/ElanXmlDigester/ElanXmlDigester.java
+++ src/main/java/mx/geoint/ElanXmlDigester/ElanXmlDigester.java
@@ -24,10 +24,11 @@ public class ElanXmlDigester {
 
     /**
      * Inicializa el path del archivo a parsear
+     *
      * @param eaf_path String, Ruta del archivo eaf
-     * @param uuid String, Identificador del usuario
+     * @param uuid     String, Identificador del usuario
      */
-    public ElanXmlDigester(String eaf_path, String uuid){
+    public ElanXmlDigester(String eaf_path, String uuid) {
         String normalize = Normalizer.normalize(eaf_path, Normalizer.Form.NFD);
         this.uuid = uuid;
         filepathEaf = normalize.replaceAll("[^\\p{ASCII}]", "");
@@ -35,11 +36,12 @@ public class ElanXmlDigester {
 
     /**
      * Inicializa el path del archivo eaf y multimedia a parsear
-     * @param eaf_path String, Ruta del archivo eaf
+     *
+     * @param eaf_path        String, Ruta del archivo eaf
      * @param multimedia_path String, Ruta del archivo multimedia
-     * @param uuid String, Identificador del usuario
+     * @param uuid            String, Identificador del usuario
      */
-    public ElanXmlDigester(String eaf_path, String multimedia_path, String uuid){
+    public ElanXmlDigester(String eaf_path, String multimedia_path, String uuid) {
         this.uuid = uuid;
         //String normalize = Normalizer.normalize(eaf_path, Normalizer.Form.NFD);
         //filepath = normalize.replaceAll("[^\\p{ASCII}]", "");
@@ -51,48 +53,60 @@ public class ElanXmlDigester {
     /**
      * Obtiene las anotaciones por medio de ParseXML, realizar los cortes de audio y genera los archivos json con la
      * información necesaria para el indexado en lucene
-     * @param tier_id String, Identificador de tier a obtener en el archivo eaf
-     * @param save_text boolean, bandera para iniciar el proceso de guardado del json
+     *
+     * @param tier_id    String, Identificador de tier a obtener en el archivo eaf
+     * @param save_text  boolean, bandera para iniciar el proceso de guardado del json
      * @param save_media boolean, bandera para iniciar el proceso de guardado de los fragmentos del multimedia
      * @throws IOException
      */
     public void parse_tier(String tier_id, boolean save_text, boolean save_media) throws IOException {
         ParseXML parseXML = new ParseXML(filepathEaf, tier_id);
+
         parseXML.read();
 
         getTier = parseXML.getTier();
         String baseNameEaf = FilenameUtils.getBaseName(filepathEaf);
-
-        if(save_media==true){
-            String type_path = getTypeMultimedia(filepathMultimedia);
-
-            for (int i = 0; i< getTier.size(); i++){
+        String type_path = getTypeMultimedia(filepathMultimedia);
+        System.out.println("type_path: "+ type_path);
+        double s[] = new double[getTier.size()];
+        double e[] = new double[getTier.size()];
+        String name[] = new String[getTier.size()];
+        if (save_media == true) {
+            for (int i = 0; i < getTier.size(); i++) {
                 Tier tier = getTier.get(i);
 
-                if(type_path.equals("wav") || type_path.equals("mp4")){
+                if (type_path.equals("wav")){
                     boolean created = false;
 
-                    switch (parseXML.getMimeType()){
-                        case "audio/x-wav":
-                            FFmpeg ffmpeg = new FFmpeg(filepathMultimedia);
-                            created = saveAudio(ffmpeg, tier, tier_id, filepathMultimedia);
-                            break;
-                        case "video/mp4":
-                            VideoCutter videoCutter = new VideoCutter();
-                            created = saveVideo(videoCutter, tier, tier_id, filepathMultimedia);
-                            break;
-                    }
+                    FFmpeg ffmpeg = new FFmpeg(filepathMultimedia);
+                    created = saveAudio(ffmpeg, tier, tier_id, filepathMultimedia);
+                    //VideoCutter videoCutter = new VideoCutter();
+                    //created = saveVideo(videoCutter, tier, tier_id, filepathMultimedia);
 
                     System.out.println(created);
-                    if(created == true && save_text == true){
+                    if (created == true && save_text == true) {
                         saveText(tier, tier_id, baseNameEaf);
                     }
 
                 }
+                if(type_path.equals("mp4")){
+                    System.out.println("video");
+                    String path = FilenameUtils.getBaseName(filepathMultimedia);
+                    String extension = FilenameUtils.getExtension(filepathMultimedia);
+
+                    s[i] = Double.parseDouble(tier.TIME_VALUE1) / 1000;
+                    e[i] = Double.parseDouble(tier.TIME_VALUE2) / 1000;
+                    name[i] = format_name(tier, tier_id, path, extension);
+                    System.out.println(s[i]);
+                    System.out.println(e[i]);
+                    System.out.println(name[i]);
+                    System.out.println("video");
+
+                }
             }
-        }else{
-            if(save_text == true){
-                for (int i = 0; i< getTier.size();i++) {
+        }else {
+            if (save_text == true) {
+                for (int i = 0; i < getTier.size(); i++) {
                     Tier tier = getTier.get(i);
                     saveText(tier, tier_id, baseNameEaf);
                 }
@@ -102,26 +116,27 @@ public class ElanXmlDigester {
 
     /**
      * Se encarga de realizar los cortes de video y guardarlo
+     *
      * @param videoCutter VideoCutter, Una instancia de la clase videoCutter
-     * @param tier Tier, Instancia de la clase tier
-     * @param tier_id String, Identificador de tier a obtener en el archivo eaf
-     * @param source String, ruta del multimedia
+     * @param tier        Tier, Instancia de la clase tier
+     * @param tier_id     String, Identificador de tier a obtener en el archivo eaf
+     * @param source      String, ruta del multimedia
      * @return
      */
-    public boolean saveVideo(VideoCutter videoCutter, Tier tier, String tier_id, String source){
-        String basePath = FilenameUtils.getPath(source)+"multimedia/";
+    public boolean saveVideo(VideoCutter videoCutter, Tier tier, String tier_id, String source) {
+        String basePath = FilenameUtils.getPath(source) + "multimedia/";
         String path = FilenameUtils.getBaseName(source);
         String type_path = FilenameUtils.getExtension(source);
 
         String file_name = format_name(tier, tier_id, path, type_path);
         boolean created = videoCutter.Cutter(source,
-                (Integer.parseInt(tier.TIME_VALUE1)/1000),
-                (Integer.parseInt(tier.TIME_VALUE2)/1000),
+                (Integer.parseInt(tier.TIME_VALUE1) / 1000),
+                (Integer.parseInt(tier.TIME_VALUE2) / 1000),
                 file_name);
 
-        if(created){
+        if (created) {
             tier.setProjectName(path);
-            tier.setMediaPath(basePath+file_name);
+            tier.setMediaPath(basePath + file_name);
             tier.setOriginalMediaPath(source);
         }
         return created;
@@ -129,26 +144,27 @@ public class ElanXmlDigester {
 
     /**
      * Se encarga de realizar los cortes de audio y guardarlo
-     * @param ffmpeg FFmpef, Una instancia de la clase FFmpeg
-     * @param tier Tier, Instancia de la clase tier
+     *
+     * @param ffmpeg  FFmpef, Una instancia de la clase FFmpeg
+     * @param tier    Tier, Instancia de la clase tier
      * @param tier_id String, Identificador de tier a obtener en el archivo eaf
-     * @param source String, ruta del multimedia
+     * @param source  String, ruta del multimedia
      * @return
      */
-    public boolean saveAudio(FFmpeg ffmpeg, Tier tier, String tier_id, String source){
-        String basePath = FilenameUtils.getPath(source)+"multimedia/";
+    public boolean saveAudio(FFmpeg ffmpeg, Tier tier, String tier_id, String source) {
+        String basePath = FilenameUtils.getPath(source) + "multimedia/";
         String path = FilenameUtils.getBaseName(source);
         String type_path = FilenameUtils.getExtension(source);
 
         String file_name = format_name(tier, tier_id, path, type_path);
         boolean created = ffmpeg.cortador(source,
-                (Integer.parseInt(tier.TIME_VALUE1)/1000),
-                (tier.DIFF_TIME/1000)+.5,
+                (Integer.parseInt(tier.TIME_VALUE1) / 1000),
+                (tier.DIFF_TIME / 1000) + .5,
                 file_name);
 
-        if(created){
+        if (created) {
             tier.setProjectName(path);
-            tier.setMediaPath(basePath+file_name);
+            tier.setMediaPath(basePath + file_name);
             tier.setOriginalMediaPath(source);
         }
         return created;
@@ -156,45 +172,48 @@ public class ElanXmlDigester {
 
     /**
      * Se encarga de realizar los json y guardalos
-     * @param tier Tier, Instancia de la clase tier
+     *
+     * @param tier    Tier, Instancia de la clase tier
      * @param tier_id String, Identificador de tier a obtener en el archivo eaf
-     * @param path String, Nombre del archivo
+     * @param path    String, Nombre del archivo
      * @throws IOException
      */
     public void saveText(Tier tier, String tier_id, String path) throws IOException {
-        String file_name_json = format_name(tier, tier_id, path,"json");
+        String file_name_json = format_name(tier, tier_id, path, "json");
         Gson gson = new Gson();
 
         String basePath = FilenameUtils.getPath(filepathEaf);
         String currentDirectory = existDirectory(basePath);
 
-        FileWriter file = new FileWriter( currentDirectory + file_name_json);
+        FileWriter file = new FileWriter(currentDirectory + file_name_json);
         file.write(gson.toJson(tier));
         file.close();
     }
 
     /**
      * Se encarga de formatear el nombre a utilizar de los audio, videos y json
-     * @param tier Tier, Instancia de la clase tier
-     * @param tier_id String, Identificador de tier a obtener en el archivo eaf
-     * @param path String, Nombre del archivo
+     *
+     * @param tier      Tier, Instancia de la clase tier
+     * @param tier_id   String, Identificador de tier a obtener en el archivo eaf
+     * @param path      String, Nombre del archivo
      * @param type_file String, extension del archivo
      * @return
      */
-    public String format_name(Tier tier, String tier_id, String path, String type_file){
+    public String format_name(Tier tier, String tier_id, String path, String type_file) {
         String name_file = String.format("%s_%s_%s_%s_%s_%s_%s.%s", tier.ANNOTATION_ID, tier_id, tier.TIME_SLOT_REF1, tier.TIME_SLOT_REF2, tier.TIME_VALUE1, tier.TIME_VALUE2, path, type_file);
         return name_file;
     }
 
     /**
-     *  Regresa la ruta del directorio a crear o existente
+     * Regresa la ruta del directorio a crear o existente
+     *
      * @param pathDirectory String, Directorio a crear
      * @return
      */
-    private String existDirectory(String pathDirectory){
+    private String existDirectory(String pathDirectory) {
         String currentDirectory = pathDirectory + "file_to_index/";
 
-        if(!Files.exists(Path.of(currentDirectory))){
+        if (!Files.exists(Path.of(currentDirectory))) {
             File newSubDirectory = new File(currentDirectory);
             newSubDirectory.mkdirs();
         }
@@ -203,11 +222,12 @@ public class ElanXmlDigester {
     }
 
     /**
-     *  regresa la extension del archivo multimedia normalizada y en minusculas
+     * regresa la extension del archivo multimedia normalizada y en minusculas
+     *
      * @param filepathMultimedia String, tipo de extension del multimedia
      * @return
      */
-    private String getTypeMultimedia(String filepathMultimedia){
+    private String getTypeMultimedia(String filepathMultimedia) {
         String type_path = FilenameUtils.getExtension(filepathMultimedia);
         String normalize = Normalizer.normalize(type_path, Normalizer.Form.NFD);
         String type_path_normalize = normalize.replaceAll("[^\\p{ASCII}]", "");
@@ -216,18 +236,16 @@ public class ElanXmlDigester {
     }
 
     /**
-     *
      * @return String, Identificador del usuario
      */
-    public String getUUID(){
+    public String getUUID() {
         return uuid;
     }
 
     /**
-     *
      * @return String, Directorio donde se encuentran los json
      */
-    public String basePathJsonFiles(){
+    public String basePathJsonFiles() {
         String basePath = FilenameUtils.getPath(filepathEaf);
         return existDirectory(basePath);
     }
