c9209ac6b112e5d09f4f57c034df7f547bce0a2a
unknown <ucp.jose@gmail.com>
2022-08-12 16:45:13 -0500

    Agregado controlador y servicio para stream de video

---
+++ src/main/java/mx/geoint/Streaming/StreamingService.java
@@ -0,0 +1,24 @@
+package mx.geoint.Streaming;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class StreamingService {
+    private static final String FORMAT = "classpath:public/Files/multimedia/%s.mp4";
+    private static final String AUDIO_FORMAT = "classpath:public/Files/multimedia/%s.wav";
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    public Mono<Resource> getVideo(String title){
+        return Mono.fromSupplier(() -> this.resourceLoader.getResource(String.format(FORMAT, title)));
+    }
+
+    public Mono<Resource> getAudio(String title){
+        return Mono.fromSupplier(() -> this.resourceLoader.getResource(String.format(AUDIO_FORMAT, title)));
+    }
+}
