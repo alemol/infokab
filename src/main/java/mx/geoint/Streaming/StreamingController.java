c025665f0228f00ccd5b18659865cd0cfbc0a1c1
unknown <ucp.jose@gmail.com>
2022-08-12 16:45:13 -0500

    Agregado controlador y servicio para stream de video
    Former-commit-id: c9209ac6b112e5d09f4f57c034df7f547bce0a2a

---
+++ src/main/java/mx/geoint/Streaming/StreamingController.java
@@ -0,0 +1,27 @@
+package mx.geoint.Streaming;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class StreamingController {
+    @Autowired
+    private StreamingService streamingService;
+
+    @GetMapping(value = "video/{title}", produces = "video/mp4")
+    public Mono<Resource> getVideo(@PathVariable String title, @RequestHeader("Range") String range){
+        System.out.println(range);
+        return streamingService.getVideo(title);
+    }
+
+    @GetMapping(value = "audio/{title}", produces = "audio/wav")
+    public Mono<Resource> getAudio(@PathVariable String title, @RequestHeader("Range") String range){
+        System.out.println(range);
+        return streamingService.getAudio(title);
+    }
+}
