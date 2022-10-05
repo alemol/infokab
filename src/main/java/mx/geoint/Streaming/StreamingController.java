18b5cd7cc21868659f7f6a820e600d010a94e35f
sergio <sage_sergio@hotmail.com>
2022-10-05 10:05:34 -0500

    Actualización de  crossOrigin en apis

--- src/main/java/mx/geoint/Streaming/StreamingController.java
+++ src/main/java/mx/geoint/Streaming/StreamingController.java
@@ -9,7 +9,7 @@ import reactor.core.publisher.Mono;
 
 import javax.servlet.http.HttpServletRequest;
 
-@CrossOrigin(origins = {"http://localhost:3009", "http://localhost:3000", "http://10.2.102.182:3009", "http://10.2.102.182"})
+@CrossOrigin(origins = {"http://infokaab.com/","http://infokaab.com.mx/","http://localhost:3009", "http://localhost:3000", "http://10.2.102.182:3009","http://10.2.102.182"})
 @RestController
 @RequestMapping(path="api/streaming")
 public class StreamingController {
