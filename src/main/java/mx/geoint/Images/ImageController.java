7e04d23b1b60332037c28fc235ad180cadc7f7bc
ulises <umorales@centrogeo.edu.mx>
2022-12-01 14:54:43 -0600

    eliminar imagenes del servidor

--- src/main/java/mx/geoint/Images/ImageController.java
+++ src/main/java/mx/geoint/Images/ImageController.java
@@ -1,7 +1,6 @@
 package mx.geoint.Images;
 
 
-import org.apache.commons.io.FilenameUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +8,7 @@ import java.io.File;
 
 import static mx.geoint.pathSystem.DIRECTORY_PROJECTS;
 
-//@CrossOrigin(origins = {"http://infokaab.com/","http://infokaab.com.mx/","http://localhost:3009", "http://localhost:3000", "http://10.2.102.182:3009","http://10.2.102.182"})
+@CrossOrigin(origins = {"http://infokaab.com/", "http://infokaab.com.mx/", "http://localhost:3009", "http://localhost:3000", "http://10.2.102.182:3009", "http://10.2.102.182"})
 @RestController
 @RequestMapping(path = "api/images")
 public class ImageController {
@@ -24,29 +23,18 @@ public class ImageController {
      *
      * @return ArraList<String> Lista de los nombre de los archivos
      */
-    @RequestMapping(path = "/readpath", method = RequestMethod.POST, consumes = "application/json")
-    //@PostMapping(value = "/updatePerson", consumes = "application/json", produces = "application/json")
+    @RequestMapping(path = "/deleteImage", method = RequestMethod.POST, consumes = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public GalleryModel Filelist(@RequestBody routeModel route) {
-        String dir = route.getRoute() + "/Images/";
-
-        int lastIndex = 0;
-        String[] pathnames;
-
-        File f = new File(dir);
-
-        pathnames = f.list();
-
-        for (String pathname : pathnames) {
-            String x = FilenameUtils.getBaseName(pathname);
-            lastIndex = Integer.parseInt(x.split("image")[1]);
+    public boolean Filelist(@RequestBody routeModel route) {
+        String dir = DIRECTORY_PROJECTS+ route.getRoute().split("Project/")[1];
+        Boolean deleted = false;
+        try {
+            File f = new File(dir); //file to be delete
+            deleted = f.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        GalleryModel galleryModel = new GalleryModel();
-        galleryModel.setimageList(pathnames);
-        galleryModel.setLastIndex(lastIndex);
-        System.out.println(pathnames.length);
-
-        return galleryModel;
+        return deleted;
     }
 
 }
