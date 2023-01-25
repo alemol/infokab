d75a9706cfa5340ad2ee2a55abf18080577f1ba8
sergio <sage_sergio@hotmail.com>
2023-01-25 09:43:57 -0600

    cors agregado

--- src/main/java/mx/geoint/Apis/Project/ProjectController.java
+++ src/main/java/mx/geoint/Apis/Project/ProjectController.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Map;
 
-//@CrossOrigin(origins = {"http://infokaab.com/","http://infokaab.com.mx/","http://localhost:3009", "http://localhost:3000", "http://10.2.102.182:3009","http://10.2.102.182"})
+@CrossOrigin(origins = {"http://infokaab.com/","http://infokaab.com.mx/","http://localhost:3009", "http://localhost:3000", "http://10.2.102.182:3009","http://10.2.102.182"})
 @RestController
 @RequestMapping(path = "api/project")
 public class ProjectController {
